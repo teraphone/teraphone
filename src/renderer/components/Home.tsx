caccf703df426c08da3806ca324793a88fa89790
David Wurtz <davidjwurtz@gmail.com>
2022-02-23 17:32:45 -0800

    suport token refresh

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button';
 import useAuth from '../hooks/useAuth';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
@@ -5,28 +6,47 @@ const Home = () => {
   const axiosPrivate = useAxiosPrivate();
   const auth = useAuth();
 
-  // eslint-disable-next-line no-console
-  console.log(`zztoken`);
-  // eslint-disable-next-line no-console
-  console.log(auth.state.token);
+  const setAuthExpire = () => {
+    const { token } = auth.state;
+    auth.setState({ token, expiration: 0 });
+  };
 
-  axiosPrivate
-    .get('/v1/private')
-    .then((response) => {
-      // eslint-disable-next-line no-console
-      console.log(response);
-      return true;
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log(error);
+  const setAuthExpireSoon = () => {
+    const { token } = auth.state;
+    auth.setState({ token, expiration: Math.floor(Date.now() / 1000) + 1000 });
+  };
 
-      return false;
-    });
+  const pingPrivateWelcome = () => {
+    axiosPrivate
+      .get('/v1/private')
+      .then((response) => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+        return true;
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+
+        return false;
+      });
+  };
 
   return (
     <div>
       <h1>Home</h1>
+      <p>Unix time: {Math.floor(Date.now() / 1000)}</p>
+      <p>Expiration: {auth.state.expiration}</p>
+      <p>Token: {auth.state.token}</p>
+      <Button variant="contained" onClick={setAuthExpire}>
+        Set Auth Expire
+      </Button>
+      <Button variant="contained" onClick={setAuthExpireSoon}>
+        Set Auth Expire Soon
+      </Button>
+      <Button variant="contained" onClick={pingPrivateWelcome}>
+        Ping Private Welcome
+      </Button>
     </div>
   );
 };
--- src/renderer/hooks/useAxiosPrivate.ts
+++ src/renderer/hooks/useAxiosPrivate.ts
@@ -1,18 +1,47 @@
 import * as React from 'react';
-import { axiosPrivate } from '../api/axios';
+import { useNavigate } from 'react-router-dom';
+import axios, { axiosPrivate } from '../api/axios';
 import useAuth from './useAuth';
 
 const useAxiosPrivate = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
 
   const updateAuthHeader = () => {
+    const refreshAuth = async () => {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${auth.state.token}`,
+        },
+      };
+      const response = await axios.get('/v1/private/auth', config);
+      const { token, expiration } = response.data;
+      auth.setState({ token, expiration });
+    };
+
+    const isExpired = auth.state.expiration - Math.floor(Date.now() / 1000) < 0;
+    const expiresSoon =
+      auth.state.expiration - Math.floor(Date.now() / 1000) < 3600 &&
+      !isExpired;
+
+    if (isExpired) {
+      // eslint-disable-next-line no-console
+      console.log('WARNING: AUTH TOKEN EXPIRED!!!');
+      navigate('/');
+    }
+    if (expiresSoon) {
+      // eslint-disable-next-line no-console
+      console.log('WARNING: AUTH TOKEN EXPIRING SOON. REFRESHING...');
+      refreshAuth();
+    }
+
     const requestIntercept = axiosPrivate.interceptors.request.use(
       (config) => {
         if (!config.headers) {
           config.headers = {};
         }
         // eslint-disable-next-line no-console
-        console.log(`Bearer ${auth.state.token}`);
+        console.log(`updateAuthHeader, time: ${Math.floor(Date.now() / 1000)}`);
         config.headers.Authorization = `Bearer ${auth.state.token}`;
 
         return config;
@@ -26,8 +55,8 @@ const useAxiosPrivate = () => {
     };
   };
 
-  updateAuthHeader(); // initialize header
-  React.useEffect(updateAuthHeader, [auth.state]);
+  // initAuthHeader();
+  React.useEffect(updateAuthHeader, [auth, navigate]);
 
   return axiosPrivate;
 };

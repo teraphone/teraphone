f8751da9f7d42357d95723114ca1673754c7e431
David Wurtz <davidjwurtz@gmail.com>
2022-03-03 15:42:00 -0800

    first attempt at livekit integration

--- src/renderer/components/GroupRoom.tsx
+++ src/renderer/components/GroupRoom.tsx
@@ -4,17 +4,37 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import GroupsIcon from '@mui/icons-material/Groups';
 import * as React from 'react';
+import * as Livekit from 'livekit-client';
 import * as models from '../models/models';
 import RoomParticipants from './RoomParticipants';
 
+const connectConfigMeta: Livekit.RoomConnectOptions = {
+  autoSubscribe: true,
+};
+
+const connectConfig: Livekit.RoomConnectOptions = {};
+
 function GroupRoom(props: { roominfo: models.RoomInfo }) {
   const { roominfo } = props;
-  const { room, users } = roominfo;
+  const { room, users, token } = roominfo;
   const { id, name, group_id: groupId } = room;
+  const livekitRoom = React.useMemo(() => {
+    return new Livekit.Room();
+  }, []);
+
+  // TODO: this isn't right. probably need to use livekit react library
+
+  React.useEffect(() => {
+    const url = 'wss://demo.dally.app';
+
+    livekitRoom.connect(url, token, { autoSubscribe: true });
+    // setLivekitRoom(new Livekit.Room());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleClick = React.useCallback(() => {
-    console.log('clicked room', roominfo);
-  }, [roominfo]);
+    console.log('clicked room', roominfo, livekitRoom);
+  }, [roominfo, livekitRoom]);
 
   return (
     <>

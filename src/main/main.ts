f9c4f564243f0224eb758fee9f651935b36294f3
David Wurtz <davidjwurtz@gmail.com>
2022-03-10 14:41:04 -0800

    smaller window by default

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 400,
+    height: 600,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),

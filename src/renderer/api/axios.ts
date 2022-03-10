08f8d8912c7e55ccc05418edd38ac79a5fc37af2
David Wurtz <davidjwurtz@gmail.com>
2022-03-10 14:37:20 -0800

    use remote db api

--- src/renderer/api/axios.ts
+++ src/renderer/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://api.dally.app';
 
 export const axiosPublic = axios.create({
   baseURL: BASE_URL,

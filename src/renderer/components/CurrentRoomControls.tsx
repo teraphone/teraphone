5d427041316c24b94f5d814df29e5e4f16a238dd
David Wurtz <davidjwurtz@gmail.com>
2022-03-11 17:46:08 -0800

    connection status

--- src/renderer/components/CurrentRoomControls.tsx
+++ src/renderer/components/CurrentRoomControls.tsx
@@ -2,29 +2,90 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import useCurrentRoom from 'renderer/hooks/useCurrentRoom';
+import PendingIcon from '@mui/icons-material/Pending';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import useRoom from '../hooks/useRoom';
 
 function CurentRoomControls() {
   const { currentRoom } = useCurrentRoom();
-  const isActive = currentRoom.roomId !== 0 && currentRoom.roomId !== undefined;
+  const { isConnecting, error } = useRoom();
+  const isActive: boolean =
+    currentRoom.roomId !== 0 && currentRoom.roomId !== undefined;
   const msg = isActive
     ? `${currentRoom.groupName} / ${currentRoom.roomName}`
     : 'none';
+
+  const StatusConnected = () => {
+    return (
+      <Typography variant="body1" sx={{ color: 'success.light' }}>
+        <SignalCellularAltIcon
+          fontSize="medium"
+          sx={{
+            mb: -0.5,
+
+            color: 'success.light',
+          }}
+        />
+        {' Voice Connected'}
+      </Typography>
+    );
+  };
+
+  const StatusConnecting = () => {
+    return (
+      <Typography variant="body1" sx={{ color: 'warning.light' }}>
+        <PendingIcon
+          fontSize="medium"
+          sx={{
+            mb: -0.5,
+
+            color: 'warning.light',
+          }}
+        />
+        {' Voice Connecting'}
+      </Typography>
+    );
+  };
+
+  const StatusError = () => {
+    return (
+      <Typography variant="body1" sx={{ color: 'error.light' }}>
+        <ErrorOutlineIcon
+          fontSize="medium"
+          sx={{
+            mb: -0.5,
+
+            color: 'error.light',
+          }}
+        />
+        {' Error Connecting'}
+      </Typography>
+    );
+  };
+
+  const Status = () => {
+    if (error) {
+      return <StatusError />;
+    }
+    if (isConnecting) {
+      return <StatusConnecting />;
+    }
+    return <StatusConnected />;
+  };
 
   console.log('currentRoom', currentRoom);
   console.log('isActive', isActive);
   console.log('msg', msg);
 
   return (
-    <Box sx={{}}>
-      <Typography
-        variant="body2"
-        sx={{
-          color: 'text.secondary',
-        }}
-      >
-        Room Name: {msg}.
-      </Typography>
-    </Box>
+    isActive && (
+      <Box sx={{}}>
+        <Status />
+
+        <Typography variant="body2">Room Name: {msg}.</Typography>
+      </Box>
+    )
   );
 }
 

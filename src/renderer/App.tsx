5401d355c335ff6bb28695248cae0f48e19d6ca5
David Wurtz <davidjwurtz@gmail.com>
2022-03-31 18:19:55 -0700

    app provider

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
@@ -9,30 +10,33 @@ import { CurrentRoomProvider } from './contexts/CurrentRoomContext';
 import { ConnectionProvider } from './contexts/ConnectionContext';
 import { FirebaseProvider } from './contexts/FirebaseContext';
 import { AppUserProvider } from './contexts/AppUserContext';
+import { store } from './redux/store';
 
 export default function App() {
   return (
-    <AppUserProvider>
-      <AuthProvider>
-        <MuteProvider>
-          <RoomProvider>
-            <CurrentRoomProvider>
-              <ConnectionProvider>
-                <FirebaseProvider>
-                  <Router>
-                    <Routes>
-                      <Route path="/" element={<SignIn />} />
-                      <Route path="/signin" element={<SignIn />} />
-                      <Route path="/signup" element={<SignUp />} />
-                      <Route path="/home" element={<Home />} />
-                    </Routes>
-                  </Router>
-                </FirebaseProvider>
-              </ConnectionProvider>
-            </CurrentRoomProvider>
-          </RoomProvider>
-        </MuteProvider>
-      </AuthProvider>
-    </AppUserProvider>
+    <Provider store={store}>
+      <AppUserProvider>
+        <AuthProvider>
+          <MuteProvider>
+            <RoomProvider>
+              <CurrentRoomProvider>
+                <ConnectionProvider>
+                  <FirebaseProvider>
+                    <Router>
+                      <Routes>
+                        <Route path="/" element={<SignIn />} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/home" element={<Home />} />
+                      </Routes>
+                    </Router>
+                  </FirebaseProvider>
+                </ConnectionProvider>
+              </CurrentRoomProvider>
+            </RoomProvider>
+          </MuteProvider>
+        </AuthProvider>
+      </AppUserProvider>
+    </Provider>
   );
 }

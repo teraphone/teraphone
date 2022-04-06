cb9d55978113f6a8760f93adb1376d276eff4afd
David Wurtz <davidjwurtz@gmail.com>
2022-04-05 18:48:16 -0700

    unknownParticipant listener middleware

--- src/renderer/redux/Middleware.ts
+++ src/renderer/redux/Middleware.ts
@@ -1,7 +1,13 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit';
 import { onValue, ref } from 'firebase/database';
-import { addParticipantRTListener, setGroup } from './ArtySlice';
+import {
+  addParticipantRTListener,
+  setGroup,
+  unknownParticipant,
+} from './ArtySlice';
 import * as models from '../models/models';
+import { getGroupUserInfo, getRoomUserInfo } from './WorldSlice';
+import type { RootState } from './store';
 
 const listenerMiddleware = createListenerMiddleware();
 
@@ -13,6 +19,24 @@ listenerMiddleware.startListening({
     onValue(nodeRef, (snapshot) => {
       const rooms = snapshot.val() as models.ParticipantRTRooms;
       listenerApi.dispatch(setGroup({ groupId, rooms }));
+    });
+  },
+});
+
+listenerMiddleware.startListening({
+  actionCreator: unknownParticipant,
+  effect: (action, listenerApi) => {
+    const { client, groupId, userId } = action.payload;
+    listenerApi.dispatch(getGroupUserInfo({ client, groupId, userId }));
+    const state = listenerApi.getState() as RootState;
+    const { rooms } = state.world.groups.find(
+      (groupInfo) => groupInfo.group.id === groupId
+    ) as models.GroupInfo;
+    rooms.forEach((roomInfo) => {
+      const { id: roomId } = roomInfo.room;
+      listenerApi.dispatch(
+        getRoomUserInfo({ client, groupId, roomId, userId })
+      );
     });
   },
 });

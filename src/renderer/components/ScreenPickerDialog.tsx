b0cb79bb1bd23780af44f68170a73a2809cedc99
David Wurtz <davidjwurtz@gmail.com>
2022-04-19 18:13:57 -0700

    added tabs to picker dialog. WIP

--- src/renderer/components/ScreenPickerDialog.tsx
+++ src/renderer/components/ScreenPickerDialog.tsx
@@ -2,6 +2,9 @@
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { Tab } from '@mui/material';
+import { TabContext, TabList, useTabContext } from '@mui/lab';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import {
   setScreens,
@@ -13,19 +16,37 @@ import {
 } from '../redux/ScreenShareSlice';
 import useRoom from '../hooks/useRoom';
 
+function ScreenPickerTabPanel(props: {
+  children: React.ReactNode;
+  value: string;
+}) {
+  const { children, value: id } = props;
+  const context = useTabContext();
+  if (context === null) {
+    throw new TypeError('No TabContext provided');
+  }
+  const tabId = context.value;
+  return <div hidden={tabId !== id}>{children}</div>;
+}
+
 function ScreenPickerDialog() {
   const dispatch = useAppDispatch();
   const { room } = useRoom();
-  const pickerVisible = useAppSelector(selectPickerVisible);
   const [screenSources, setScreenSources] = React.useState<
     Electron.DesktopCapturerSource[]
   >([]);
   const [windowSources, setwindowSources] = React.useState<
     Electron.DesktopCapturerSource[]
   >([]);
+  const pickerVisible = useAppSelector(selectPickerVisible);
+  const [tabId, setTabId] = React.useState('tab1');
 
-  const handleClose = () => {
+  const handleDialogClose = () => {
     dispatch(setPickerVisible(false));
+  };
+
+  const handleTabChange = (_event: React.SyntheticEvent, id: string) => {
+    setTabId(id);
   };
 
   React.useEffect(() => {
@@ -56,10 +77,26 @@ function ScreenPickerDialog() {
   }, [pickerVisible]);
 
   return (
-    <Dialog fullScreen open={pickerVisible} onClose={handleClose}>
-      <Button autoFocus color="inherit" onClick={handleClose}>
-        close
-      </Button>
+    <Dialog fullScreen open={pickerVisible} onClose={handleDialogClose}>
+      <Box>
+        <TabContext value={tabId}>
+          <TabList value={tabId} variant="standard" onChange={handleTabChange}>
+            <Tab value="tab1" label="Applications" />
+            <Tab value="tab2" label="Screens" />
+          </TabList>
+          <ScreenPickerTabPanel value="tab1">
+            <span>This is the Applications tab</span>
+          </ScreenPickerTabPanel>
+          <ScreenPickerTabPanel value="tab2">
+            <span>This is the Screens tab</span>
+          </ScreenPickerTabPanel>
+        </TabContext>
+      </Box>
+      <Box>
+        <Button autoFocus color="inherit" onClick={handleDialogClose}>
+          close
+        </Button>
+      </Box>
     </Dialog>
   );
 }

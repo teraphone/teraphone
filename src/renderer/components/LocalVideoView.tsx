c579d09cb38b84c9bb49f340e10bf49a4c42db05
David Wurtz <davidjwurtz@gmail.com>
2022-04-21 17:44:46 -0700

    local video view. WIP

---
+++ src/renderer/components/LocalVideoView.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import {
+  setScreens,
+  setWindows,
+  setPickerVisible,
+  selectScreens,
+  selectWindows,
+  selectPickerVisible,
+  ScreenSource,
+} from '../redux/ScreenShareSlice';
+import useRoom from '../hooks/useRoom';
+
+function LocalVideoView() {
+  const dispatch = useAppDispatch();
+  const { room } = useRoom();
+  const screens = useAppSelector(selectScreens);
+  const windows = useAppSelector(selectWindows);
+  return (
+    <div>
+      <div>Screens: {JSON.stringify(screens)}</div>
+      <div>Windows: {JSON.stringify(windows)}</div>
+    </div>
+  );
+}
+
+export default LocalVideoView;

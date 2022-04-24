c972e60c0ca96536cbef433d065780e494a2a7aa
David Wurtz <davidjwurtz@gmail.com>
2022-04-23 17:20:23 -0700

    cleanup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
-import LocalVideoView from './components/LocalVideoView';
 import { RoomProvider } from './contexts/RoomContext';
 import { FirebaseProvider } from './contexts/FirebaseContext';
 import { store } from './redux/store';
@@ -19,7 +18,6 @@ export default function App() {
               <Route path="/signin" element={<SignIn />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/localvideo" element={<LocalVideoView />} />
             </Routes>
           </Router>
         </FirebaseProvider>

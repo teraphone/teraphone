ac282bf660d933a1db15ae405ed51b3ed9c16d24
David Wurtz <davidjwurtz@gmail.com>
2022-04-26 18:02:43 -0700

    borrow from erb

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare let window: Window;
-
 export type SerializedDesktopCapturerSource = {
   id: string;
   name: string;
@@ -9,26 +6,25 @@ export type SerializedDesktopCapturerSource = {
   appIconDataURL: string | null;
 };
 
-export interface IElectron {
-  ipcRenderer: {
-    myPing(): void;
-    queryScreens(options: {
-      types: string[]; // "screen" | "window"
-      thumbnailSize?: {
-        width: number;
-        height: number;
-      };
-      fetchWindowIcons?: boolean;
-    }): Promise<SerializedDesktopCapturerSource[]>;
-    on(channel: any, func: any): void;
-    once(channel: any, func: any): void;
-  };
-}
-
 declare global {
   interface Window {
-    electron: IElectron;
-    process: any;
-    require: any;
+    electron: {
+      ipcRenderer: {
+        myPing(): void;
+        queryScreens(options: {
+          types: string[]; // "screen" | "window"
+          thumbnailSize?: {
+            width: number;
+            height: number;
+          };
+          fetchWindowIcons?: boolean;
+        }): Promise<SerializedDesktopCapturerSource[]>;
+        on(
+          channel: string,
+          func: (...args: unknown[]) => void
+        ): (() => void) | undefined;
+        once(channel: string, func: (...args: unknown[]) => void): void;
+      };
+    };
   }
 }

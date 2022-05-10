cb27b7fa7feb88d3ad1e23e18b4865956dfa98a0
David Wurtz <davidjwurtz@gmail.com>
2022-05-10 14:41:46 -0700

    rename

--- src/renderer/components/VideoGridOverlay.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-function VideoGridOverlay() {
-  return null;
-}
-
-export default VideoGridOverlay;
---
+++ src/renderer/components/VideoOverlay.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function VideoOverlay() {
+  return null;
+}
+
+export default VideoOverlay;

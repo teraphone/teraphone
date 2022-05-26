72b487b88a1db624cb66eb882d3959e503b560a7
David Wurtz <davidjwurtz@gmail.com>
2022-05-26 14:46:09 -0700

    online and offline contacts

--- src/renderer/components/GroupContacts.tsx
+++ src/renderer/components/GroupContacts.tsx
@@ -6,7 +6,13 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
+import { Collapse } from '@mui/material';
 import * as models from '../models/models';
+import { useAppSelector, useAppDispatch } from '../redux/hooks';
+import { selectAppUser } from '../redux/AppUserSlice';
+import { selectOnline, unknownParticipant } from '../redux/ArtySlice';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 export interface GroupContactsProps {
   groupInfo: models.GroupInfo;
@@ -14,9 +20,105 @@ export interface GroupContactsProps {
 
 function GroupContacts(props: GroupContactsProps) {
   const { groupInfo } = props;
-  const { users } = groupInfo;
+  const { users, group } = groupInfo;
+  const { appUser } = useAppSelector(selectAppUser);
+  const online = useAppSelector(selectOnline);
+  const onlineGroup = online[group.id];
+  const dispatch = useAppDispatch();
+  const axiosPrivate = useAxiosPrivate();
+  const [usersObj, setUsersObj] = React.useState(
+    {} as { [id: number]: models.GroupUserInfo }
+  );
+  const [onlineOpen, setOnlineOpen] = React.useState(true);
+  const [offlineOpen, setOfflineOpen] = React.useState(false);
 
-  const contacts = users.map((user) => {
+  const handleOnlineClick = React.useCallback(() => {
+    setOnlineOpen(!onlineOpen);
+  }, [onlineOpen]);
+
+  const handleOfflineClick = React.useCallback(() => {
+    setOfflineOpen(!offlineOpen);
+  }, [offlineOpen]);
+
+  React.useEffect(() => {
+    setUsersObj(
+      users.reduce((acc, user) => ({ ...acc, [user.user_id]: user }), {})
+    );
+  }, [users]);
+
+  React.useEffect(() => {
+    if (onlineGroup) {
+      Object.entries(onlineGroup).forEach(([userId]) => {
+        if (!usersObj[+userId]) {
+          dispatch(
+            unknownParticipant({
+              client: axiosPrivate,
+              groupId: group.id,
+              userId: +userId,
+            })
+          );
+        }
+      });
+    }
+  }, [axiosPrivate, dispatch, group.id, onlineGroup, usersObj]);
+
+  let thisUser = {} as models.GroupUserInfo;
+  const onlineUsers = [] as models.GroupUserInfo[];
+  const offlineUsers = [] as models.GroupUserInfo[];
+
+  [...users]
+    .sort((a, b) => (a.name > b.name ? 1 : -1))
+    .forEach((user) => {
+      if (user.user_id === appUser.id) {
+        thisUser = user;
+      } else if (onlineGroup && onlineGroup[user.user_id]) {
+        onlineUsers.push(user);
+      } else {
+        offlineUsers.push(user);
+      }
+    });
+
+  console.log('onlineUsers', onlineUsers);
+  console.log('offlineUsers', offlineUsers);
+
+  const thisContact = (
+    <ListItemButton dense component="li" key={thisUser.user_id}>
+      <ListItemIcon>
+        <Avatar sx={{ width: 20, height: 20, fontSize: 14 }}>
+          {thisUser.name[0]}
+        </Avatar>
+      </ListItemIcon>
+      <ListItemText
+        primary={thisUser.name}
+        primaryTypographyProps={{
+          variant: 'body2',
+          noWrap: true,
+        }}
+      />
+    </ListItemButton>
+  );
+
+  const onlineContacts = onlineUsers.map((user) => {
+    const { user_id: userId, name } = user;
+    return (
+      <ListItemButton dense component="li" key={userId}>
+        <ListItemIcon>
+          <Avatar sx={{ width: 20, height: 20, fontSize: 14 }}>
+            {name[0]}
+          </Avatar>
+        </ListItemIcon>
+        <ListItemText
+          primary={name}
+          primaryTypographyProps={{
+            variant: 'body2',
+            noWrap: true,
+          }}
+        />
+      </ListItemButton>
+    );
+  });
+
+  const offlineContacts = offlineUsers.map((user) => {
     const { user_id: userId, name } = user;
     return (
       <ListItemButton dense component="li" key={userId}>
@@ -58,7 +160,27 @@ function GroupContacts(props: GroupContactsProps) {
           Group Contacts
         </Typography>
       </Box>
-      <List disablePadding>{contacts}</List>
+      <List disablePadding>
+        <ListItemButton onClick={handleOnlineClick}>
+          <ListItemText primary="Online" />
+          {onlineOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={onlineOpen} timeout="auto" unmountOnExit>
+          <List disablePadding>
+            {thisContact}
+            {onlineContacts}
+          </List>
+        </Collapse>
+      </List>
+      <List disablePadding>
+        <ListItemButton onClick={handleOfflineClick}>
+          <ListItemText primary="Offline" />
+          {offlineOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={offlineOpen} timeout="auto" unmountOnExit>
+          <List disablePadding>{offlineContacts}</List>
+        </Collapse>
+      </List>
     </Box>
   );
 }

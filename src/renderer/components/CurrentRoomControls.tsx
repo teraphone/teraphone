6bf5472cf83e6d6011b36322f244daf6215201bc
David Wurtz <davidjwurtz@gmail.com>
2022-05-30 11:41:28 -0700

    fix tooltip stuttering

--- src/renderer/components/CurrentRoomControls.tsx
+++ src/renderer/components/CurrentRoomControls.tsx
@@ -25,6 +25,156 @@ import { selectCurrentRoom } from '../redux/CurrentRoomSlice';
 import { setPickerVisible } from '../redux/ScreenShareSlice';
 import '../lib/ExtendedLocalParticipant';
 
+const StatusConnected = () => {
+  return (
+    <Typography variant="body1" sx={{ color: 'success.light' }}>
+      <SignalCellularAltIcon
+        fontSize="medium"
+        sx={{
+          mb: -0.5,
+
+          color: 'success.light',
+        }}
+      />
+      {' Voice Connected'}
+    </Typography>
+  );
+};
+
+const StatusConnecting = () => {
+  return (
+    <Typography variant="body1" sx={{ color: 'warning.light' }}>
+      <CircularProgress size={16} sx={{ mx: 0.5, color: 'warning.light' }} />
+      {'  Voice Connecting'}
+    </Typography>
+  );
+};
+
+const StatusError = () => {
+  return (
+    <Typography variant="body1" sx={{ color: 'error.light' }}>
+      <ErrorOutlineIcon
+        fontSize="medium"
+        sx={{
+          mb: -0.5,
+
+          color: 'error.light',
+        }}
+      />
+      {' Error Connecting'}
+    </Typography>
+  );
+};
+
+const Status = (props: { status: ConnectionStatus }) => {
+  const { status } = props;
+  switch (status) {
+    case ConnectionStatus.Connected: {
+      return <StatusConnected />;
+    }
+    case ConnectionStatus.Connecting: {
+      return <StatusConnecting />;
+    }
+    case ConnectionStatus.Error: {
+      return <StatusError />;
+    }
+    case ConnectionStatus.Reconnecting: {
+      return <StatusConnecting />;
+    }
+    default: {
+      return <></>;
+    }
+  }
+};
+
+const ShareCameraButton = (props: {
+  status: ConnectionStatus;
+  onClick: () => void;
+}) => {
+  const { status, onClick } = props;
+  return (
+    <Tooltip title="Share Camera" placement="top" arrow>
+      <span>
+        <IconButton
+          color="primary"
+          aria-label="disconnect"
+          component="span"
+          onClick={onClick}
+          disabled
+        >
+          <VideoCameraFrontIcon />
+        </IconButton>
+      </span>
+    </Tooltip>
+  );
+};
+
+const ShareScreenButton = (props: {
+  status: ConnectionStatus;
+  onClick: () => void;
+}) => {
+  const { status, onClick } = props;
+  return (
+    <Tooltip title="Share Screen" placement="top" arrow>
+      <span>
+        <IconButton
+          color="primary"
+          aria-label="disconnect"
+          component="span"
+          onClick={onClick}
+          disabled={status !== ConnectionStatus.Connected}
+        >
+          <ScreenShareIcon />
+        </IconButton>
+      </span>
+    </Tooltip>
+  );
+};
+
+const DisconnectButton = (props: {
+  status: ConnectionStatus;
+  onClick: () => void;
+}) => {
+  const { status, onClick } = props;
+  return (
+    <Tooltip title="Disconnect" placement="top" arrow>
+      <span>
+        <IconButton
+          color="primary"
+          aria-label="disconnect"
+          component="span"
+          onClick={onClick}
+          disabled={status !== ConnectionStatus.Connected}
+        >
+          <LogoutIcon />
+        </IconButton>
+      </span>
+    </Tooltip>
+  );
+};
+
+const InfoButton = (props: {
+  status: ConnectionStatus;
+  onClick: () => void;
+}) => {
+  const { status, onClick } = props;
+  return (
+    <Tooltip title="Info" placement="top" arrow>
+      <span>
+        <IconButton
+          color="primary"
+          aria-label="info"
+          component="span"
+          onClick={onClick}
+          disabled={status !== ConnectionStatus.Connected}
+        >
+          <InfoIcon />
+        </IconButton>
+      </span>
+    </Tooltip>
+  );
+};
+
 function CurentRoomControls() {
   const dispatch = useAppDispatch();
   const { currentRoom } = useAppSelector(selectCurrentRoom);
@@ -38,163 +188,46 @@ function CurentRoomControls() {
   );
   const debug = false;
 
-  const StatusConnected = () => {
-    return (
-      <Typography variant="body1" sx={{ color: 'success.light' }}>
-        <SignalCellularAltIcon
-          fontSize="medium"
-          sx={{
-            mb: -0.5,
+  const handleShareCameraClick = React.useCallback(() => {
+    console.log('handleShareCameraClick');
+  }, []);
 
-            color: 'success.light',
-          }}
-        />
-        {' Voice Connected'}
-      </Typography>
-    );
-  };
+  const handleShareScreenClick = React.useCallback(() => {
+    dispatch(setPickerVisible(true));
+  }, [dispatch]);
 
-  const StatusConnecting = () => {
-    return (
-      <Typography variant="body1" sx={{ color: 'warning.light' }}>
-        <CircularProgress size={16} sx={{ mx: 0.5, color: 'warning.light' }} />
-        {'  Voice Connecting'}
-      </Typography>
-    );
-  };
+  const handleDisconnectClick = React.useCallback(() => {
+    room?.disconnect();
+    remove(userRTRef);
+  }, [room, userRTRef]);
 
-  const StatusError = () => {
-    return (
-      <Typography variant="body1" sx={{ color: 'error.light' }}>
-        <ErrorOutlineIcon
-          fontSize="medium"
-          sx={{
-            mb: -0.5,
-
-            color: 'error.light',
-          }}
-        />
-        {' Error Connecting'}
-      </Typography>
-    );
-  };
-
-  const Status = () => {
-    switch (connectionStatus) {
-      case ConnectionStatus.Connected: {
-        return <StatusConnected />;
-      }
-      case ConnectionStatus.Connecting: {
-        return <StatusConnecting />;
-      }
-      case ConnectionStatus.Error: {
-        return <StatusError />;
-      }
-      case ConnectionStatus.Reconnecting: {
-        return <StatusConnecting />;
-      }
-      default: {
-        return <></>;
-      }
-    }
-  };
-
-  const ShareCameraButton = () => {
-    return (
-      <Tooltip title="Share Camera" placement="top" arrow>
-        <span>
-          <IconButton
-            color="primary"
-            aria-label="disconnect"
-            component="span"
-            onClick={() => {
-              alert('Not implemented yet.');
-            }}
-            disabled
-          >
-            <VideoCameraFrontIcon />
-          </IconButton>
-        </span>
-      </Tooltip>
-    );
-  };
-
-  const ShareScreenButton = () => {
-    const enabled = room?.localParticipant.isScreenShareEnabled;
-    return (
-      <Tooltip title="Share Screen" placement="top" arrow>
-        <span>
-          <IconButton
-            color="primary"
-            aria-label="disconnect"
-            component="span"
-            onClick={() => {
-              dispatch(setPickerVisible(true));
-            }}
-            disabled={connectionStatus !== ConnectionStatus.Connected}
-          >
-            <ScreenShareIcon />
-          </IconButton>
-        </span>
-      </Tooltip>
-    );
-  };
-
-  const DisconnectButton = () => {
-    return (
-      <Tooltip title="Disconnect" placement="top" arrow>
-        <span>
-          <IconButton
-            color="primary"
-            aria-label="disconnect"
-            component="span"
-            onClick={() => {
-              room?.disconnect();
-              remove(userRTRef);
-              console.log('room', room);
-            }}
-            disabled={connectionStatus !== ConnectionStatus.Connected}
-          >
-            <LogoutIcon />
-          </IconButton>
-        </span>
-      </Tooltip>
-    );
-  };
-
-  const InfoButton = () => {
-    const handleClick = () => {
-      console.log('room', room);
-    };
-    return (
-      <Tooltip title="Info" placement="top" arrow>
-        <span>
-          <IconButton
-            color="primary"
-            aria-label="info"
-            component="span"
-            onClick={handleClick}
-            disabled={connectionStatus !== ConnectionStatus.Connected}
-          >
-            <InfoIcon />
-          </IconButton>
-        </span>
-      </Tooltip>
-    );
-  };
+  const handleInfoClick = React.useCallback(() => {
+    console.log('room', room);
+  }, [room]);
 
   return (
     <>
       {connectionStatus !== ConnectionStatus.Disconnected && (
         <Box sx={{}}>
-          <Status />
+          <Status status={connectionStatus} />
 
           <Typography variant="body2">{`${currentRoom.groupName} / ${currentRoom.roomName}`}</Typography>
           <Box>
-            <ShareCameraButton />
-            <ShareScreenButton />
-            <DisconnectButton />
-            {debug && <InfoButton />}
+            <ShareCameraButton
+              status={connectionStatus}
+              onClick={handleShareCameraClick}
+            />
+            <ShareScreenButton
+              status={connectionStatus}
+              onClick={handleShareScreenClick}
+            />
+            <DisconnectButton
+              status={connectionStatus}
+              onClick={handleDisconnectClick}
+            />
+            {debug && (
+              <InfoButton status={connectionStatus} onClick={handleInfoClick} />
+            )}
           </Box>
         </Box>
       )}

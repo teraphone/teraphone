1e772d65ad949b04d825ed28d6c889b252b251a5
David Wurtz <davidjwurtz@gmail.com>
2022-05-30 20:00:49 -0700

    export

--- src/renderer/redux/CurrentRoomSlice.ts
+++ src/renderer/redux/CurrentRoomSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from './store';
 
-type CurrentRoomState = {
+export type CurrentRoomState = {
   roomId: number;
   roomName: string;
   groupId: number;

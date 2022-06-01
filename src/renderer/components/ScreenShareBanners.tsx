63c146db3127bd75bb4b51d6d55aefd2d2906d45
David Wurtz <davidjwurtz@gmail.com>
2022-05-31 18:59:54 -0700

    ScreenShareBanners WIP

---
+++ src/renderer/components/ScreenShareBanners.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Box, Avatar, IconButton, Tooltip, Typography } from '@mui/material';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
+import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
+import {
+  setScreens,
+  setWindows,
+  setPickerVisible,
+  selectScreens,
+  selectWindows,
+  selectPickerVisible,
+  ScreenSource,
+  validDataURL,
+} from '../redux/ScreenShareSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { SerializedDesktopCapturerSource } from '../global';
+
+function ScreenShareBanner(props: {
+  source: SerializedDesktopCapturerSource;
+  setSources: ActionCreatorWithPayload<ScreenSource, string>;
+}) {
+  const { source, setSources } = props;
+  const { id, name, appIconDataURL } = source;
+  const isValidIcon = appIconDataURL ? validDataURL(appIconDataURL) : false;
+
+  return (
+    <Box>
+      {isValidIcon ? (
+        <Avatar src={appIconDataURL as string} />
+      ) : (
+        <Avatar>{name[0]}</Avatar>
+      )}
+      <Typography variant="body2">{name}</Typography>
+      <Tooltip title="Stop Sharing" placement="top" arrow>
+        <IconButton onClick={() => console.log('clicked', id)}>
+          <StopScreenShareIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+}
+
+function ScreenShareBanners() {
+  const screens = useAppSelector(selectScreens);
+  const windows = useAppSelector(selectWindows);
+
+  return null;
+}
+
+export default ScreenShareBanners;

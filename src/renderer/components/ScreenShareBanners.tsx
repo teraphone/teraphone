5c27f2a0baa1d0c9dfe7b2679941f5c23f547467
David Wurtz <davidjwurtz@gmail.com>
2022-06-01 11:28:27 -0700

    styling

--- src/renderer/components/ScreenShareBanners.tsx
+++ src/renderer/components/ScreenShareBanners.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
-import { Box, Avatar, IconButton, Tooltip, Typography } from '@mui/material';
+import {
+  Box,
+  Avatar,
+  IconButton,
+  Tooltip,
+  Typography,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from '@mui/material';
 import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import {
@@ -22,21 +32,82 @@ function ScreenShareBanner(props: {
   const { source, setSources } = props;
   const { id, name, appIconDataURL } = source;
   const isValidIcon = appIconDataURL ? validDataURL(appIconDataURL) : false;
+  const avatarDim = '24px';
 
   return (
-    <Box>
-      {isValidIcon ? (
-        <Avatar src={appIconDataURL as string} />
-      ) : (
-        <Avatar>{name[0]}</Avatar>
-      )}
-      <Typography variant="body2">{name}</Typography>
-      <Tooltip title="Stop Sharing" placement="top" arrow>
-        <IconButton onClick={() => console.log('clicked', id)}>
-          <StopScreenShareIcon />
-        </IconButton>
-      </Tooltip>
-    </Box>
+    <ListItem
+      disableGutters
+      disablePadding
+      sx={{
+        pl: '0px',
+        pr: '40px',
+        py: '2px',
+        my: '2px',
+        backgroundColor: '#f8f8f8',
+      }}
+      secondaryAction={
+        <Box
+          sx={{
+            pr: '8px',
+          }}
+        >
+          <Tooltip title="Stop Sharing" placement="right" arrow>
+            <IconButton
+              size="small"
+              aria-label="stop-sharing"
+              component="span"
+              onClick={() => console.log('clicked', id)}
+            >
+              <StopScreenShareIcon
+                sx={{
+                  color: 'black',
+                }}
+                fontSize="small"
+              />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      }
+    >
+      <ListItemAvatar
+        sx={{
+          px: '4px',
+          minWidth: avatarDim,
+        }}
+      >
+        {isValidIcon ? (
+          <Avatar
+            sx={{
+              height: avatarDim,
+              width: avatarDim,
+            }}
+            variant="square"
+            src={appIconDataURL as string}
+          />
+        ) : (
+          <Avatar
+            sx={{
+              height: avatarDim,
+              width: avatarDim,
+              fontSize: '14',
+            }}
+            variant="square"
+          >
+            {name[0]}
+          </Avatar>
+        )}
+      </ListItemAvatar>
+      <ListItemText
+        primary={name}
+        primaryTypographyProps={{
+          variant: 'body2',
+          noWrap: true,
+          sx: {
+            fontSize: 12,
+          },
+        }}
+      />
+    </ListItem>
   );
 }
 
@@ -53,10 +124,16 @@ function ScreenShareBanners() {
   ));
 
   return (
-    <Box>
+    <List
+      dense
+      sx={{
+        boxSizing: 'border-box',
+        mx: '2px',
+      }}
+    >
       {screenBanners}
       {windowBanners}
-    </Box>
+    </List>
   );
 }
 

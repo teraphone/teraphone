9829bae75f587377726f7ea8d193d95b4c50c8bd
David Wurtz <davidjwurtz@gmail.com>
2022-07-11 16:53:12 -0700

    extended useRoom with disconnect function

--- src/renderer/contexts/RoomContext.tsx
+++ src/renderer/contexts/RoomContext.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable no-console */
 
 import * as React from 'react';
-import { useRoom, RoomState } from '@livekit/react-core';
+import { useRoom, RoomState } from '../lib/ExtendedUseRoom';
 
 const RoomContext = React.createContext({} as RoomState);
 
 export const RoomProvider: React.FC = ({ children }) => {
   const roomState = useRoom();
-  React.useEffect(() => {
-    console.log('RoomProvider Mounted', roomState);
-    return () => console.log('RoomProvider Unmounted');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return (
     <RoomContext.Provider value={roomState}>{children}</RoomContext.Provider>
   );

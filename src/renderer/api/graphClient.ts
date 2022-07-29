9749962f6554be426670d7f77d96c658acd5a257
David Wurtz <davidjwurtz@gmail.com>
2022-07-28 18:10:04 -0700

    add graphClient.ts

---
+++ src/renderer/api/graphClient.ts
@@ -0,0 +1,42 @@
+import {
+  AuthenticationProvider,
+  AuthenticationProviderOptions,
+  Client,
+} from '@microsoft/microsoft-graph-client';
+
+class GraphAuthProvider implements AuthenticationProvider {
+  scopes = ['User.Read'];
+
+  constructor(scopes?: string[]) {
+    if (scopes) {
+      this.scopes = scopes;
+    }
+  }
+
+  public async getAccessToken(
+    authenticationProviderOptions?: AuthenticationProviderOptions
+  ): Promise<string> {
+    if (authenticationProviderOptions?.scopes) {
+      this.scopes = authenticationProviderOptions.scopes;
+      // ^^^ this.scopes is never used
+    }
+    // todo: add scopes support auth()
+    const authResult = await window.electron.ipcRenderer.auth();
+    try {
+      if (authResult) {
+        return authResult.accessToken;
+      }
+      throw new Error('Auth result is null');
+    } catch (err: unknown) {
+      throw new Error('Error getting access token');
+    }
+  }
+}
+
+const authProvider = new GraphAuthProvider();
+const client = Client.initWithMiddleware({
+  debugLogging: true,
+  authProvider,
+});
+
+export default client;

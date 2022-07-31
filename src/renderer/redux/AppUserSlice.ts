f5ea57022576bee2cffbea4ac15d627d0b3a2ce5
David Wurtz <davidjwurtz@gmail.com>
2022-07-31 15:10:56 -0700

    add tenantUser to state

--- src/renderer/redux/AppUserSlice.ts
+++ src/renderer/redux/AppUserSlice.ts
@@ -1,9 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User } from '../models/models';
+import { TenantUser, User } from '../models/models';
 import type { RootState } from './store';
 
 type AppUserState = {
   appUser: User;
+  tenantUser: TenantUser;
 };
 
 const initialState: AppUserState = {
@@ -14,6 +15,7 @@ const initialState: AppUserState = {
     name: '',
     email: '',
   },
+  tenantUser: {} as TenantUser,
 };
 
 export const appUserSlice = createSlice({
@@ -23,11 +25,16 @@ export const appUserSlice = createSlice({
     setAppUser: (state, action: PayloadAction<User>) => {
       state.appUser = action.payload;
     },
+    setTenantUser: (state, action: PayloadAction<TenantUser>) => {
+      state.tenantUser = action.payload;
+    },
   },
 });
 
-export const { setAppUser } = appUserSlice.actions;
+export const { setAppUser, setTenantUser } = appUserSlice.actions;
 
 export const selectAppUser = (state: RootState) => state.appUser;
+
+export const selectTenantUser = (state: RootState) => state.appUser.tenantUser;
 
 export default appUserSlice.reducer;

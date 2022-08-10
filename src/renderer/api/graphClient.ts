fa1b95e2e6e52fa75d96c7adde66a1fecb8ba5b5
David Wurtz <davidjwurtz@gmail.com>
2022-08-09 20:49:12 -0700

    add helpers

--- src/renderer/api/graphClient.ts
+++ src/renderer/api/graphClient.ts
@@ -4,6 +4,41 @@ import {
   Client,
 } from '@microsoft/microsoft-graph-client';
 
+export const b64toBlob = async (
+  b64Data: string,
+  contentType = 'image/png',
+  sliceSize = 512
+): Promise<Blob> => {
+  const byteCharacters: string = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i += 1) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+};
+
+export const blobToBase64 = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+    reader.readAsDataURL(blob);
+  });
+};
+
 class GraphAuthProvider implements AuthenticationProvider {
   scopes = ['User.Read'];
 

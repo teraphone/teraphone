6176c4aebc4dddf6285ad673f0a816b4802f0a21
David Wurtz <davidjwurtz@gmail.com>
2022-08-12 11:54:06 -0700

    hackish. set playback device on connect

--- src/renderer/components/TeamRoom.tsx
+++ src/renderer/components/TeamRoom.tsx
@@ -21,6 +21,7 @@ import PeekRoomParticipants from './PeekRoomParticipants';
 import { selectCurrentRoom, setCurrentRoom } from '../redux/CurrentRoomSlice';
 import { setWindowOpen, selectWindowOpen } from '../redux/VideoViewSlice';
 import { selectMute } from '../redux/MuteSlice';
+import { selectSelectedSpeakerId } from '../redux/SettingsSlice';
 
 function TeamRoom(props: {
   teamInfo: models.TeamInfo;
@@ -39,6 +40,7 @@ function TeamRoom(props: {
     connectionStatus === ConnectionStatus.Connected;
   const isVideoWindowOpen = useAppSelector(selectWindowOpen);
   const mute = useAppSelector(selectMute);
+  const selectedSpeakerId = useAppSelector(selectSelectedSpeakerId);
 
   React.useEffect(() => {
     console.log('TeamRoom', roomInfo.room.displayName, 'Mounted');
@@ -81,8 +83,12 @@ function TeamRoom(props: {
         if (mute) {
           micPublication?.audioTrack?.mute().catch(console.error);
         }
+        // set playback device
+        if (selectedSpeakerId !== '') {
+          room?.switchActiveDevice('audiooutput', selectedSpeakerId);
+        }
       } catch (error) {
-        console.error('publish mic error:', error);
+        console.error(error);
       }
     } catch (error) {
       console.error(error);
@@ -91,10 +97,12 @@ function TeamRoom(props: {
     connect,
     dispatch,
     mute,
+    room,
     roomInfo.room.displayName,
     roomInfo.room.id,
     roomInfo.room.teamId,
     roomInfo.roomToken,
+    selectedSpeakerId,
     teamInfo.team.displayName,
   ]);
 
